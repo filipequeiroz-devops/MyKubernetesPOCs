0fd3591a84448f87c26d3f1d3392c0f8b6635caf
Filipe <filipe.queiroz.abreu@gmail.com>
2026-07-22 06:28:16 -0300

    jobs

--- gitpush.ps1
+++ gitpush.ps1
@@ -5,3 +5,7 @@ param (
 git add * #adciona todos os arquivos para o commit
 git commit -m "$commitMessage" #realiza o commit com a mensagem fornecida
 git push MKP master #envia as alterações para o repositório remoto
+
+start-sleep -Seconds 5 #aguarda 5 segundos para garantir que o push seja concluído antes de abrir o navegador
+
+git status #exibe o status do repositório após o push
